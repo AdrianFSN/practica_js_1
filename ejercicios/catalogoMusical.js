799a85c13efef5ddfa05b901d62e7ede7fb58392
Adrián S <adriansotes@gmail.com>
2023-12-19 14:58:58 +0100

    feat: traigo todo el trabajo hecho en el repositorio de pruebas, para que haya trackeo de commits en el repositorio correcto

--- ejercicios/catalogoMusical.js
+++ ejercicios/catalogoMusical.js
@@ -15,53 +15,121 @@ consola todas las canciones de ese género.
 Calcular Promedio de Duración: Calcula y muestra en la consola el promedio de la
 duración de todas las canciones en el catálogo. (opcional)*/
 
+const objetoCatalogo = {
+    listaCanciones: [
+        {
+            titulo: 'Qué puedo hacer',
+            genero: 'Pop',
+            duracion: 3.05
+        },
+        {
+            titulo: 'Toxicity',
+            genero: 'Metal',
+            duracion: 3.38
+        },
+        {
+            titulo: 'Aunque es de noche',
+            genero: 'Flamenco',
+            duracion: 3.05
+        },
+        {
+            titulo: 'Lo que dicta el corazón',
+            genero: 'Pop',
+            duracion: 3.42
+        },
+        {
+            titulo: 'Country girl',
+            genero: 'Rock',
+            duracion: 4.31
+        }
 
-const crearCancion = () => {
-    let cancion = {
-        titulo: prompt('Por favor, introduce el título de la canción'),
-        genero: prompt('Por favor, introduce el género de la canción'),
-        duracion: parseFloat(prompt('Por favor, introduce la duración de la canción (en número y en minutos.segundos)'), 10)
-    };
-    return cancion;
-};
-//console.log(crearCancion())
+    ],
+    sacarListaCanciones: listarCatalogo = () => {
+        const headers = ['TÍTULO', 'GÉNERO', 'DURACIÓN'];
+        let tablaCatalogo = [headers];
+        objetoCatalogo.listaCanciones
+            .forEach(item => tablaCatalogo.push([item.titulo, item.genero, item.duracion]));
+        return tablaCatalogo.join('\n');
 
-const crearCatalogo = () => {
-    let listaCanciones = [];
-    let exit = false
-    while (!exit) {
-        let continuar = prompt('¿Quieres añadir una canción al catálogo? (S, N)')
-        if (continuar !== null) {
-            continuar = continuar.toUpperCase()
-            if (continuar === 'S') {
-                listaCanciones.push(crearCancion());
-            } else if (continuar === 'N') {
-                alert('Paramos de agregar canciones');
-                exit = true;
-            } else { console.log('Solo puedes contestar S o N') };
-        } else {
-            alert('Operación cancelada');
-            exit = true
+    },
+    crearCatalogo: llenarCatalogo = () => {
+        let cancionesAñadidas = [];
+        const crearCancion = () => {
+            let cancion = {
+                titulo: prompt('Por favor, introduce el título de la canción'),
+                genero: prompt('Por favor, introduce el género de la canción'),
+                duracion: parseFloat(prompt('Por favor, introduce la duración de la canción (en número y en minutos.segundos)'), 10)
+            };
+            return cancion;
         };
 
-    };
-    return listaCanciones;
-};
-console.log(crearCatalogo());
+        let exit = false
+        while (!exit) {
+            let continuar = prompt('¿Quieres añadir una canción al catálogo? (S, N)')
+            if (continuar !== null) {
+                continuar = continuar.toUpperCase()
+                if (continuar === 'S') {
+                    cancionesAñadidas.push(crearCancion());
+                } else if (continuar === 'N') {
+                    alert('Paramos de agregar canciones');
+                    exit = true;
+                } else { console.log('Solo puedes contestar S o N') };
+            } else {
+                alert('Operación cancelada');
+                exit = true
+            };
 
-
-/*const crearCatalogo = () => {
-    let listaCanciones = [];
-    const agregarCancion = (titulo, genero, duracion) => {
-        {
-            titulo,
-                genero,
-                duracion
-        }
-        listaCanciones.push(agregarCancion);
+        };
+        return objetoCatalogo.listaCanciones.concat(cancionesAñadidas);
+    },
+    filtrarXGenero: filtradoGenero = () => {
+        let elegirGenero = prompt('Introduce el género musical por el que quieres filtrar');
+        if (elegirGenero !== null) {
+            elegirGenero = elegirGenero.toLowerCase();
+            const listaFiltrada = objetoCatalogo.listaCanciones
+                .filter(elemento => elemento.genero.toLowerCase() === elegirGenero);
+            if (listaFiltrada.length > 0 ? console.log(listaFiltrada) : alert('No hay resultados para tu búsqueda'));
+        } else {
+            alert('Operación cancelada');
+        };
+    },
+    calcularMedia: calculoDuracionMedia = () => {
+        const duracionCatalogo = objetoCatalogo.listaCanciones
+            .reduce((acum, item) =>
+                (acum += item.duracion), 0);
+        return {
+            duracionTotal: duracionCatalogo,
+            duraccionMedia: duracionCatalogo / objetoCatalogo.listaCanciones.length
+        };
     }
-    return listaCanciones;
-    //const listarCanciones = función
-    //const buscarXGenero = función
-    //const calcularPromedioDuración = función
-*/
+};
+
+const miCatalogo = objetoCatalogo;
+
+let userChoice = prompt(`
+¿Qué deseas hacer en el catálogo?
+Elige una opción:
+
+- Listar todas las canciones (L)
+- Filtrar canciones por género (G)
+- Añadir nuevas canciones (A)
+- Calcular la duración media de las canciones (M)
+
+`);
+
+if (userChoice !== null) {
+    userChoice = userChoice.toUpperCase();
+    if (userChoice === 'L') {
+        console.log(miCatalogo.sacarListaCanciones());
+    } else if (userChoice === 'A') {
+        console.log(miCatalogo.crearCatalogo());
+    } else if (userChoice === 'G') {
+        console.log(miCatalogo.filtrarXGenero());
+    } else if (userChoice === 'M') {
+        console.log(miCatalogo.calcularMedia());
+    } else {
+        console.log('Ninguna opción válida elegida')
+    }
+} else {
+    console.log('Has cancelado la acción');
+};
