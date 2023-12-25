61a9b009bff2ac0731b533c0ca1e680b67801514
Adrián S <adriansotes@gmail.com>
2023-12-25 17:07:20 +0100

    feat: tras createMatch, hago la función pointWonBy

--- ejercicios/wimblecode.js
+++ ejercicios/wimblecode.js
@@ -2,7 +2,7 @@
 const NUM_JUGADORES_NECESARIOS = 4;
 
 // Métodos
-const apuntarJugadores = (numParticipantes = NUM_JUGADORES_NECESARIOS) => {
+const inscribirJugadores = (numParticipantes = NUM_JUGADORES_NECESARIOS) => {
     let listaJugadores = [];
     while (listaJugadores.length < numParticipantes) {
         let nombreJugador = prompt('Introduce un nombre para el jugador: ');
@@ -25,15 +25,16 @@ const formarParejas = (pareja, lista) => {
     return pareja;
 };
 
-const limpiarListaJugadores = (pareja, lista) => {
-    return lista.filter(jugador => !pareja.includes(jugador));
-};
+
 
 const crearEmparejamientos = (lista) => {
     let copiaLista = lista.slice();
     let listaParejas = [];
     let parejasNecesarias = (NUM_JUGADORES_NECESARIOS / 2);
     let unaPareja = [];
+    const limpiarListaJugadores = (pareja, unaLista) => {
+        return unaLista.filter(jugador => !pareja.includes(jugador));
+    };
 
     for (let i = 0; i < parejasNecesarias; i++) {
         listaParejas.push(formarParejas(unaPareja, copiaLista));
@@ -45,30 +46,59 @@ const crearEmparejamientos = (lista) => {
 
 const createMatch = (jugadorA, jugadorB) => {
     let rivalA = {
+        id: 1,
         nombre: jugadorA,
         evolucionPuntos: [],
         totalPuntos: 0
 
     };
     let rivalB = {
+        id: 2,
         nombre: jugadorB,
         evolucionPuntos: [],
         totalPuntos: 0
     };
+    let marcadorRonda = [
+        rivalA.totalPuntos,
+        rivalB.totalPuntos
+    ];
+    const pointWonBy = (id) => {
+        let punto = 1;
+        if (id === rivalA.id) {
+            console.log(`¡Punto para ${rivalA.nombre}!`)
+            rivalA.totalPuntos += punto;
+        } else {
+            console.log(`¡Punto para ${rivalB.nombre}!`)
+            rivalB.totalPuntos += punto;
+        };
+        marcadorRonda = [
+            rivalA.totalPuntos,
+            rivalB.totalPuntos
+        ];
+    };
+    //let numeroAleatrorio = Math.floor(Math.random() * 2) + 1;
+    //console.log(numeroAleatrorio);
+    //console.log(pointWonBy(numeroAleatrorio));
+    //console.log(pointWonBy(Math.floor(Math.random() * 2) + 1));
+    pointWonBy(Math.floor(Math.random() * 2) + 1);
 
-    return [rivalA, rivalB];
+    return [rivalA, rivalB, marcadorRonda];
 };
 
 // Torneo
-const jugadoresInscritos = apuntarJugadores(); // deben ser 4
+const jugadoresInscritos = inscribirJugadores(); // deben ser 4
 console.log(`Has inscrito a 4 jugadores: 
 ${jugadoresInscritos.join('\n')}
 Puedes iniciar el torneo.`);
 
 const cuadroTorneo = crearEmparejamientos(jugadoresInscritos);
+console.log(`Este es el cuadro del torneo:
+Partido 1: ${cuadroTorneo[0][0]} vs. ${cuadroTorneo[0][1]}
+Partido 2: ${cuadroTorneo[1][0]} vs. ${cuadroTorneo[1][1]}`)
 
-const game = createMatch('Alberto C', 'David J');
-console.log(game);
+const game = createMatch(cuadroTorneo[0][0], cuadroTorneo[0][1]);
+//const game = createMatch('Alberto C', 'David J');
+console.log('Esto es game: ', game[0], game[1], game[2]);
 
 
 
