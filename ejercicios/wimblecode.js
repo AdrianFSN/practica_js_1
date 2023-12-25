4d7f53d83bafd24aee71603a00979c2cb993bf50
Adrián S <adriansotes@gmail.com>
2023-12-25 09:57:49 +0100

    fix: la función emparejamientos ya no devuelve duplicados

--- ejercicios/wimblecode.js
+++ ejercicios/wimblecode.js
@@ -1,5 +1,5 @@
 //Settings
-const NUM_JUGADORES_NECESARIOS = 8;
+const NUM_JUGADORES_NECESARIOS = 4;
 
 // Métodos
 const apuntarJugadores = () => {
@@ -9,7 +9,9 @@ const apuntarJugadores = () => {
         listaJugadores.push(nombreJugador);
     }
     if (listaJugadores.length === NUM_JUGADORES_NECESARIOS) {
-        console.log('Has inscrito a 4 jugadores. Puedes iniciar el torneo');
+        console.log(`Has inscrito a 4 jugadores: 
+${listaJugadores.join('\n')}
+Puedes iniciar el torneo.`);
         return listaJugadores;
     }
 };
@@ -18,42 +20,49 @@ const formarParejas = (pareja, lista) => {
     let pointerPareja = 0;
     while (pointerPareja < 2) {
         let index = Math.floor(Math.random() * NUM_JUGADORES_NECESARIOS);
-        if (!pareja.includes(lista[index])) {
+        if (!pareja.includes(lista[index]) && index < lista.length) {
             pareja.push(lista[index]);
             pointerPareja += 1;
         }
     }
-    console.log('lo que devuelve formar parejas es: ', pareja)
     return pareja;
-}
+};
+
+const limpiarListaJugadores = (pareja, lista) => {
+    return lista.filter(jugador => !pareja.includes(jugador));
+};
+
 const crearEmparejamientos = (lista) => {
+    let copiaLista = lista.slice();
     let listaParejas = [];
     let parejasNecesarias = (NUM_JUGADORES_NECESARIOS / 2);
-
     let unaPareja = [];
 
     for (let i = 0; i < parejasNecesarias; i++) {
-        listaParejas.push(formarParejas(unaPareja, lista));
+        listaParejas.push(formarParejas(unaPareja, copiaLista));
+        copiaLista = limpiarListaJugadores(unaPareja, copiaLista);
         unaPareja = [];
     }
-    console.log('Lista de parejas es: ', listaParejas);
-    console.log('Nñumero de parejas necesarias es ', parejasNecesarias);
+    return listaParejas;
 };
 
 // Objeto torneo
 const torneo = {
     jugadoresInscritos: apuntarJugadores(), // deben ser 4
-    emparejamientos: [], //debe ser una lista de listas de 2 elementos
     partidoEnJuego: {
         rival1: '',
         rival2: '',
         marcadorRival1: 0,
         marcadorRival2: 0,
-    }
+    },
 };
 
 const miTorneo = torneo;
-//console.log(miTorneo.jugadoresInscritos);
-crearEmparejamientos(miTorneo.jugadoresInscritos);
+
+//miTorneo.jugadoresInscritos;
+console.log(`
+Este es el cuadro del torneo:
+${crearEmparejamientos(miTorneo.jugadoresInscritos).join('\n')}`);
+
 
 
