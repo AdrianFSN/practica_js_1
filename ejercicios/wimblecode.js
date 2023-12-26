9899af53c1d4f2c4ade3fea1eade9bfee6b804d6
Adrián S <adriansotes@gmail.com>
2023-12-26 12:06:52 +0100

    feat: controlo la ronda para cuando hay deuce y para cuando no hay deuce

--- ejercicios/wimblecode.js
+++ ejercicios/wimblecode.js
@@ -69,7 +69,7 @@ const createMatch = (jugadorA, jugadorB) => {
         rivalB.evolucionPuntos
     ];
 
-    const traducirPuntos = (tanteoA) => {
+    const traducirPuntosNormal = (tanteoA) => {
         if (tanteoA === 0) {
             return '0';
         } else if (tanteoA === 1) {
@@ -80,20 +80,33 @@ const createMatch = (jugadorA, jugadorB) => {
             return 40;
         } else if (tanteoA === 4) {
             return 'RONDA GANADA';
-        } else {
-            console.log('PUNTUACIÓN SUPERADA');
-        }
+        };
+    };
+
+    const traducirPuntosDeuce = (tanteoA) => {
+        if (tanteoA === 0) {
+            return '0';
+        } else if (tanteoA === 1) {
+            return 15;
+        } else if (tanteoA === 2) {
+            return 30;
+        } else if (tanteoA === 3) {
+            return 'Deuce';
+        } else if (tanteoA === 4) {
+            return 'Ventaja';
+        } else if (tanteoA === 5) {
+            return 'RONDA GANADA';
+        };
     };
     const pointWonBy = (id) => {
         let punto = 1;
         if (id === rivalA.id) {
             console.log(`¡Punto para ${rivalA.nombre}!`)
             rivalA.totalPuntos += punto;
-            rivalA.evolucionPuntos = traducirPuntos(rivalA.totalPuntos);
+
         } else {
             console.log(`¡Punto para ${rivalB.nombre}!`)
             rivalB.totalPuntos += punto;
-            rivalB.evolucionPuntos = traducirPuntos(rivalB.totalPuntos);
         };
         marcadorRonda = [
             rivalA.evolucionPuntos,
@@ -105,13 +118,69 @@ const createMatch = (jugadorA, jugadorB) => {
         console.log(`Empieza el punto entre ${rivalA.nombre} y ${rivalB.nombre}...`);
         console.log(`Está siendo un peloteo vibrante...`);
         pointWonBy(Math.floor(Math.random() * 2) + 1);
-        if (rivalA.totalPuntos > 3) {
-            rivalA.rondasGanadas += 1;
-        } else if (rivalB.totalPuntos > 3) {
-            rivalB.rondasGanadas += 1;
+        rivalA.evolucionPuntos = traducirPuntosNormal(rivalA.totalPuntos);
+        rivalB.evolucionPuntos = traducirPuntosNormal(rivalB.totalPuntos);
 
+        if (rivalA.totalPuntos === 3 && rivalB.totalPuntos === 3) {
+            console.log('¡Hay deuce!')
+            rivalA.evolucionPuntos = traducirPuntosDeuce(rivalA.totalPuntos);
+            rivalB.evolucionPuntos = traducirPuntosDeuce(rivalB.totalPuntos);
+            console.log(`Así va el marcador de esta ronda:
+${rivalA.nombre}: ${rivalA.evolucionPuntos}
+${rivalB.nombre}: ${rivalB.evolucionPuntos}`);
+
+            while (rivalA.totalPuntos !== 5 || rivalA.totalPuntos !== 5) {
+                pointWonBy(Math.floor(Math.random() * 2) + 1);
+                rivalA.evolucionPuntos = traducirPuntosDeuce(rivalA.totalPuntos);
+                rivalB.evolucionPuntos = traducirPuntosDeuce(rivalB.totalPuntos);
+
+                if (rivalA.evolucionPuntos === 'Ventaja' || rivalA.evolucionPuntos === 'RONDA GANADA') {
+                    rivalB.evolucionPuntos = 40
+                } else if (rivalB.evolucionPuntos === 'Ventaja' || rivalB.evolucionPuntos === 'RONDA GANADA') {
+                    rivalA.evolucionPuntos = 40
+                };
+
+                console.log(`HOLA: Así va el marcador de esta ronda:
+${rivalA.nombre}: ${rivalA.evolucionPuntos}
+${rivalB.nombre}: ${rivalB.evolucionPuntos}`);
+
+                if (rivalA.totalPuntos === 4 && rivalB.totalPuntos === 4) {
+                    console.log('¡Hay deuce de nuevo!')
+                    rivalA.totalPuntos -= 1;
+                    rivalB.totalPuntos -= 1;
+                    rivalA.evolucionPuntos = traducirPuntosDeuce(rivalA.totalPuntos);
+                    rivalB.evolucionPuntos = traducirPuntosDeuce(rivalB.totalPuntos);
+
+                    if (rivalA.evolucionPuntos === 'Ventaja' || rivalA.evolucionPuntos === 'RONDA GANADA') {
+                        rivalB.evolucionPuntos = 40
+                    } else if (rivalB.evolucionPuntos === 'Ventaja' || rivalB.evolucionPuntos === 'RONDA GANADA') {
+                        rivalA.evolucionPuntos = 40
+                    };
+
+                    console.log(`HOLA2 Así va el marcador de esta ronda:
+${rivalA.nombre}: ${rivalA.evolucionPuntos}
+${rivalB.nombre}: ${rivalB.evolucionPuntos}`);
+                };
+            }
+            if (rivalA.totalPuntos === 5) {
+                rivalA.rondasGanadas += 1;
+                console.log(`¡${rivalA.nombre} gana la ronda!`)
+            } else if (rivalB.totalPuntos === 5) {
+                rivalB.rondasGanadas += 1;
+                console.log(`¡${rivalB.nombre} gana la ronda!`)
+            }
+
+        } else {
+            if (rivalA.totalPuntos === 4) {
+                console.log(`¡${rivalA.nombre} gana la ronda!`)
+                rivalA.rondasGanadas += 1;
+            } else if (rivalB.totalPuntos === 4) {
+                rivalB.rondasGanadas += 1;
+                console.log(`¡${rivalB.nombre} gana la ronda!`)
+
+            };
         }
-    }
+    };
     const partido = () => {
         console.log(`¡Comienza el partido entre ${rivalA.nombre} y ${rivalB.nombre}!`);
         let salir = false;
@@ -148,6 +217,4 @@ Partido 2: ${cuadroTorneo[1][0]} vs. ${cuadroTorneo[1][1]}`)
 const game = createMatch(cuadroTorneo[0][0], cuadroTorneo[0][1]);
 //const game = createMatch('Alberto C', 'David J');
 //console.log('Vamos con el primer partido', game);
-
-
 
