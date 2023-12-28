262af4a3969d975bcf13e451cb72d4b899c3d911
Adrián S <adriansotes@gmail.com>
2023-12-28 17:54:54 +0100

    feat: rehago wimblecode. funciones createMatch, pointWonBy, simpplificadas

---
+++ ejercicios/wimblecode.js
@@ -0,0 +1,80 @@
+const NUM_JUGADORES_NECESARIOS = 4;
+let listaJugadoresDisponibles = ['Alberto C', 'David J', 'Javier M', 'Edu Aguilar'];
+
+// Métodos
+const crearEmparejamientos = (lista) => {
+    let copiaLista = lista.slice();
+    let listaParejas = [];
+    let parejasNecesarias = (NUM_JUGADORES_NECESARIOS / 2);
+    let unaPareja = [];
+    const limpiarListaJugadores = (pareja, unaLista) => {
+        return unaLista.filter(jugador => !pareja.includes(jugador));
+    };
+
+    for (let i = 0; i < parejasNecesarias; i++) {
+        listaParejas.push(formarParejas(unaPareja, copiaLista));
+        copiaLista = limpiarListaJugadores(unaPareja, copiaLista);
+        unaPareja = [];
+    }
+    return listaParejas;
+};
+
+const crearPartidos = (nombre1, nombre2) => {
+    const rivalA = Object.assign({}, jugador);
+    const rivalB = Object.assign({}, jugador);
+
+    rivalA.nombre = nombre1;
+    rivalB.nombre = nombre2;
+
+}
+
+// Objeto jugador
+const jugador = {
+    id: 0,
+    nombre: '',
+    evolucionPuntos: '',
+    totalPuntos: 0,
+    rondasGanadas: 0,
+    juegosGanados: 0,
+    resultadoJuego1: 0,
+    resultadoJuego2: 0,
+    resultadoJuego3: 0
+};
+
+
+// Objeto torneo
+const torneo = {
+    listaJugadoresInscritos: listaJugadoresDisponibles,
+    parejaJugando: undefined,
+    createMatch: (nombre1, nombre2) => {
+        const rivalA = Object.assign({}, jugador);
+        const rivalB = Object.assign({}, jugador);
+
+        rivalA.nombre = nombre1;
+        rivalA.id = 1;
+        rivalB.nombre = nombre2;
+        rivalB.id = 2;
+        torneo.parejaJugando = [rivalA, rivalB];
+        return torneo.parejaJugando;
+
+    },
+
+    pointWonBy: (id) => {
+        let punto = 1;
+
+        parejaJugando
+            .forEach(item => {
+                if (item.id === id) {
+                    item.totalPuntos += punto;
+                };
+            })
+    }
+
+};
+
+const game = torneo;
+
+console.log(game);
+
+game.createMatch('Alberto C', 'David J');
+console.log(torneo.parejaJugando);
